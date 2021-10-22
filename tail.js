454123e0a51400d5290bebeb16afc21223aaecf9
Abhiram <abhiram.satha@gmail.com>
2021-10-22 00:10:26 +0000

    pushing eqArrays

--- tail.js
+++ tail.js
@@ -20,4 +20,14 @@ tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3);
 
 
+/*
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); 
+*/
+
+assertEqual(tail(["Hello", "Lighthouse", "Labs"]).toString(), ["Lighthouse", "Labs"].toString());
+
+console.log(["Hi", '  hello'].toString());
 
