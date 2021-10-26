975886341edf03868a3e48b60c28f78541e9f6a2
Abhiram <abhiram.satha@gmail.com>
2021-10-26 23:11:39 +0000

    Finalized findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,10 +5,25 @@ const assertEqual = function(actual , expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
+const assertArraysEqual = function (arr1 , arr2) {
+  let result = true;
+
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+      return result = false;
+    } else {
+      continue;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  return result; 
+}
+
 const findKeyByValue = function (object , answer){
   let result = " ";
   for (const key in object) {
-    if (answer === object[key]) {
+    if (answer === object[key]) { //object[key] accesses the property
       result = key;
     } else {
       result = undefined;
@@ -27,5 +42,5 @@ const bestTVShowsByGenre = {
   drama:  "The Wire"
 };
 
-findKeyByValue(bestTVShowsByGenre, "The Wire");
-findKeyByValue(bestTVShowsByGenre, "That '70s Show");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
