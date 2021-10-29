89f3992bb68a11e016d8b5270b2121e448fddd44
Abhiram <abhiram.satha@gmail.com>
2021-10-29 01:48:19 +0000

    Finalized takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,12 +1,24 @@
 const takeUntil = function (array, callback) {
   for (let i = 0; i < array.length; i++) {
-    if (callback === true) {
-      array.slice(0, i)
+    if (callback(array[i]) === true) {
+      //console.log(i);
+      const array2 = array.slice(0, i)
+      return array2;
     }
+    //console.log(array[i])
   }
+
 }
 
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
 
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
 
 
 
