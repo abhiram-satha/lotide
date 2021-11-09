e5e5fc5c7715187317a7527cadff1a7790e4fa4a
Abhiram <abhiram.satha@gmail.com>
2021-11-09 00:41:40 +0000

    Refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,9 +13,6 @@ const assertArraysEqual = function (arr1 , arr2) {
   return result; 
 }
 
+module.exports = assertArraysEqual;
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
