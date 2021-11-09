88e9f71ac216dc1f1b26e34862dda024189d2cf0
Abhiram <abhiram.satha@gmail.com>
2021-11-09 01:13:41 +0000

    Created Mocha/Chai tests for headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,23 @@
+const assert = require('chai').assert;
 const head = require('../head');
 const assertEqual = require('../assertEqual');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe('#head', function() {
+  it('returns  1 for [1,2,3]', function () {
+    const actual = head([1,2,3]);
+    const expected = 1;
+
+    assert.strictEqual(actual, expected);
+  })
+  it('returns "5" for ["5"]', () => {
+    const actual = head(['5']);
+    const expected = '5';
+    assert.strictEqual(actual, expected);
+  })
+  it('returns "Hello" for "["Hello", "Lighthouse", "Labs"]', ()=> {
+    const actual = head(["Hello", "Lighthouse", "Labs"]);
+    const expected = "Hello";
+
+    assert.strictEqual(actual, expected);
+  })
+})
