81bd0d444153517d6957fff18eb5921ada3f7aee
Abhiram <abhiram.satha@gmail.com>
2021-11-09 00:35:13 +0000

    Refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual , expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = function (arr1 , arr2) {
   let result = true;
   let length = arr1.length;
@@ -25,11 +18,6 @@ const eqArrays = function (arr1 , arr2) {
   return result; 
 }
 
+module.exports = eqArrays;
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
