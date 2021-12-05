5f4eaa9223d8deefffc726fa3e9b26d9a9ec343b
Abhiram Sathasivam <82683735+abhiram-satha@users.noreply.github.com>
2021-12-05 14:53:26 -0500

    Update to this code to make it work

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,23 @@
 const eqArrays = function (arr1 , arr2) {
-  let result = true;
-  let length = arr1.length;
-  if (length < arr2.length) {
-    length = arr2.length;
+  if (arr1.length !== arr2.length) {
+    return false; 
   }
-  for (var i = 0; i < length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return result = false;
-    } else {
-      continue;
+
+  for(let i = 0; i < arr1.length; i++) {
+    if(Array.isArray(arr1[i])) {
+      for (let j = 0; j < arr1[i].length; j++) {
+        if(arr1[i][j] !== arr2[i][j]) {
+          return false;
+        }
+      }
+    }
+    else if (arr1[i] !== arr2[i]) {
+      console.log(arr1[i], arr2[i])
+      return false;
     }
   }
-  return result; 
+  
+  return true; 
 }
 
 module.exports = eqArrays;
